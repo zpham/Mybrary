8d39cd02c3f3af3bc472aaeea9053ff920353db8
kimzung <zungptk@epu.edu.vn>
2023-07-31 06:45:48 +0700

    Initial Author Routes

---
+++ models/author.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const { default: mongoose } = require('mongoose')
+
+const authorSchema = new mongoose.Schema({
+    name: {
+        type: String, 
+        required: true
+    }
+})
+
+module.exports = mongoose.model('Author', authorSchema)
